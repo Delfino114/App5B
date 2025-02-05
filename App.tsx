086ed0aae38acf1910c82c8a9c5d66fa0f4e9f8f
LMendoza70 <luis.mendoza@uthh.edu.mx>
2025-02-05 12:06:18 -0600

    App 5B

--- App.tsx
+++ App.tsx
@@ -1,10 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ActivityIndicator } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>Aplicacion movil 5B</Text>
+      <ActivityIndicator size={'large'} color={'pink'} />
+      <Text>Esta app es un ejemplo</Text>
       <StatusBar style="auto" />
     </View>
   );
