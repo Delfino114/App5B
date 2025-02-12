a319057621df28c4d625c454d171286ee36c783d
Luis Alberto Mendoza <luis.mendoza@uthh.edu.mx>
2025-02-12 12:09:55 -0600

    ok

--- App.tsx
+++ App.tsx
@@ -1,12 +1,29 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, ActivityIndicator } from 'react-native';
+import { StyleSheet, Text, View, ActivityIndicator, Alert } from 'react-native';
+import Boton from './app/Components/Boton';
+import AntDesign from '@expo/vector-icons/AntDesign';
 
 export default function App() {
   return (
     <View style={styles.container}>
+      
       <Text>Aplicacion movil 5B</Text>
+      
       <ActivityIndicator size={'large'} color={'pink'} />
+      
       <Text>Esta app es un ejemplo</Text>
+      
+      <Boton titulo='Press me...' 
+      onPress={()=>{Alert.alert('Saludos a Acuario...')}}
+      icono ={<AntDesign name="play" size={24} color="white"/>}
+      posicionIcono='izquierda'
+      variante='peligro'
+      disable={true}/>
+
+      <Boton titulo='Google' onPress={()=>{Alert.alert('Se logueo con google')}}
+        icono={<AntDesign name="googleplus" size={24} color="black"/>}
+        estilo={styles.botong}/>
+      
       <StatusBar style="auto" />
     </View>
   );
@@ -18,5 +35,8 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
-  },
+  },botong:{
+    backgroundColor:'blue'
+  }
+
 });
--- index.ts
+++ index.ts
@@ -7,4 +7,4 @@ import Plantilla from './app/screens/Plantilla';
 // registerRootComponent calls AppRegistry.registerComponent('main', () => App);
 // It also ensures that whether you load the app in Expo Go or in a native build,
 // the environment is set up appropriately
-registerRootComponent(Plantilla);
+registerRootComponent(App);
