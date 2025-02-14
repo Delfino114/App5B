6a27835d8a977c4301b73dcbd6cca6c44658cdaa
Luis Alberto Mendoza <luis.mendoza@uthh.edu.mx>
2025-02-14 12:39:04 -0600

    Calculadora

--- index.ts
+++ index.ts
@@ -3,8 +3,9 @@ import { registerRootComponent } from 'expo';
 import App from './App';
 import Login from './app/screens/Login';
 import Plantilla from './app/screens/Plantilla';
+import Calculadora from './app/screens/Calculadora';
 
 // registerRootComponent calls AppRegistry.registerComponent('main', () => App);
 // It also ensures that whether you load the app in Expo Go or in a native build,
 // the environment is set up appropriately
-registerRootComponent(App);
+registerRootComponent(Calculadora);
