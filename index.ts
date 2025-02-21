5d3ea5f68be216790013f6fc85488e941beaf3c4
Luis Alberto Mendoza <luis.mendoza@uthh.edu.mx>
2025-02-21 09:18:24 -0600

    fakestore 2

--- index.ts
+++ index.ts
@@ -5,8 +5,9 @@ import Login from './app/screens/Login';
 import Plantilla from './app/screens/Plantilla';
 import Calculadora from './app/screens/Calculadora';
 import FakeStore from './app/screens/FakeStore';
+import FakeStore2 from './app/screens/FakeStore2';
 
 // registerRootComponent calls AppRegistry.registerComponent('main', () => App);
 // It also ensures that whether you load the app in Expo Go or in a native build,
 // the environment is set up appropriately
-registerRootComponent(FakeStore);
+registerRootComponent(FakeStore2);
